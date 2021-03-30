08bbfe69523e172ede431fa73620cfbfbac7be8e
Jane Clayton <janeclayton1999@gmail.com>
2021-03-30 15:56:19 +0100

    Wrote controller stubs for all menu items

--- OrderBook/src/main/java/com/as/orderbook/controller/OrderBookController.java
+++ OrderBook/src/main/java/com/as/orderbook/controller/OrderBookController.java
@@ -5,10 +5,53 @@
  */
 package com.as.orderbook.controller;
 
+import com.as.orderbook.controller.view.OrderBookView;
+
 /**
  *
  * @author Skininho
  */
 public class OrderBookController {
-    
+    private OrderBookView view;
+    public void run() {
+        int input = 0;
+        Boolean keepRunning = true;
+        while (keepRunning) {
+            input = view.printMenuAndGetSelection();
+            switch (input) {
+                case 1:
+                    System.out.println("VIEW ORDERBOOK NOT YET IMPLEMENTED");
+                    break;
+                case 2:
+                    input = view.manageOrders();
+                    switch(input) {
+                        case 1:
+                            System.out.println("MATCH ONE ORDER NOT YET IMPLEMENTED");
+                            break;
+                        case 2:
+                            System.out.println("MATCH ALL ORDERS NOT YET IMPLEMENTED");
+                            break;
+                        case 3:
+                            System.out.println("VIEW A TRADE NOT YET IMPLEMENTED");
+                            break;
+                        case 4:
+                            System.out.println("VIEW ALL TRADES NOT YET IMPLEMENTED");
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            System.out.println("Invalid selection, returning to main menu");
+                            break;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Exiting program...");
+                    keepRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid selection");
+                    break;
+            }
+        }
+    }
 }
