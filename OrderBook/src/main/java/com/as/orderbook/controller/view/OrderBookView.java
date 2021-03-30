11dc1948c7da224cc93e621b3aa2075d4a27f67a
Jane Clayton <janeclayton1999@gmail.com>
2021-03-30 15:44:23 +0100

    Added methods for view

--- OrderBook/src/main/java/com/as/orderbook/controller/view/OrderBookView.java
+++ OrderBook/src/main/java/com/as/orderbook/controller/view/OrderBookView.java
@@ -5,10 +5,45 @@
  */
 package com.as.orderbook.controller.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Skininho
  */
 public class OrderBookView {
+    public Integer printMenuAndGetSelection() {
+        Scanner inputReader = new Scanner(System.in);
+        Boolean correctInput = false;
+        Integer input = 0;
+        while (!correctInput) {
+            System.out.println("<<Orderbook>>");
+            System.out.println("1. View Orderbook");
+            System.out.println("2. Manage Orders");
+            System.out.println("3. Exit Program");
+            input = inputReader.nextInt();
+            if (input > 0 && input < 4) {
+                correctInput = true;
+            }
+        }
+        return input;
+    }
     
+    public Integer manageOrders() {
+        Scanner inputReader = new Scanner(System.in);
+        Boolean correctInput = false;
+        Integer input = 0;
+        while (!correctInput) {
+            System.out.println("1. Match One Order");
+            System.out.println("2. Match All Orders");
+            System.out.println("3. View A Trade");
+            System.out.println("4. View All Trades (Sorted by exection time)");
+            System.out.println("5. Return To Menu");
+            input = inputReader.nextInt();
+            if (input > 0 && input < 6) {
+                correctInput = true;
+            }
+        }
+        return input;
+    }
 }
